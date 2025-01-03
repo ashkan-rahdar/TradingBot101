a3b8463556089c4bae15a187d92f8b8c1a891279
ashkan-rahdar <ashkanrahdar@gmail.com>
2025-01-03 20:50:32 +0330

    custom timeframe from user fo fetching data

--- functions/fetching_data.py
+++ functions/fetching_data.py
@@ -12,6 +12,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 with open("./config.json", "r") as file:
     config = json.load(file)
 
+timeframe_mapping = {
+    "M1": mt.TIMEFRAME_M1,
+    "M5": mt.TIMEFRAME_M5,
+    "M15": mt.TIMEFRAME_M15,
+    "M30": mt.TIMEFRAME_M30,
+    "H1": mt.TIMEFRAME_H1,
+    "H4": mt.TIMEFRAME_H4,
+    "D1": mt.TIMEFRAME_D1,
+    "W1": mt.TIMEFRAME_W1,
+    "MN1": mt.TIMEFRAME_MN1,
+}
+
 from functions.logger import logger
 
 async def initialize_mt5():
@@ -19,7 +31,8 @@ async def initialize_mt5():
         logger.error("MetaTrader5 initialization failed")
         raise RuntimeError("MetaTrader5 initialization failed")
 
-async def login_mt5(config) -> mt.AccountInfo:
+async def login_mt5() -> mt.AccountInfo:
+    global config
     if not mt.login(config["account_info"]["login"], config["account_info"]["password"], config["account_info"]["server"]):
         logger.error("MetaTrader5 login failed")
         raise RuntimeError("MetaTrader5 login failed")
@@ -27,22 +40,23 @@ async def login_mt5(config) -> mt.AccountInfo:
     else:
         return mt.account_info()
 
-async def fetch_data():
+async def fetch_data(timeframe = "M15"):
+    global timeframe_mapping
+    selected_timeframe = timeframe_mapping.get(timeframe, None)
     try:
-        DataSet = pd.DataFrame(mt.copy_rates_from('EURUSD', mt.TIMEFRAME_M15, datetime.now(), 10000))
+        DataSet = pd.DataFrame(mt.copy_rates_from('EURUSD', selected_timeframe, datetime.now(), 10000))
         DataSet['time'] = pd.to_datetime(DataSet["time"], unit='s')
-        print(Fore.GREEN + Style.BRIGHT + "||||||||||||||||||| Data successfully fetched |||||||||||||||||||" + Style.RESET_ALL)
+        print(Fore.GREEN + Style.BRIGHT + f"||||||||||||||||||| Data {timeframe} successfully fetched |||||||||||||||||||" + Style.RESET_ALL)
         return DataSet
     except Exception as e:
         logger.error(f"Failed to fetch data: {e}")
         raise RuntimeError(f"Failed to fetch data: {e}")
 
-async def main_fetching_data():
+async def main_fetching_data(timeframe = 'M15' ):
     try:
         await initialize_mt5()
-        global config
-        account_info = await login_mt5(config)
-        data = await fetch_data()
+        account_info = await login_mt5()
+        data = await fetch_data(timeframe)
         return data, account_info
     except Exception as e:
         logger.error(f"An error occurred: {e}")
